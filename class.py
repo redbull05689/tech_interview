005b51d6bcf18f355f491e0a66eb7cad64159411
Maksim Vasilyev <maksim.vasilyev@wearevolt.com>
2024-08-09 15:45:11 +0300

    8

--- class.py
+++ class.py
@@ -1,13 +1,13 @@
 class Point():
+    # def __new__(cls, *args, **kwarg) : #Необязательный метод
+    #     print("вызов __new__ для" + str(cls))
+    #     return super().__new__(cls)
+
 
     def __init__(self, x, y):
         # print("вызов инит")
         self.x = x
         self.y = y
 
-    def __del__(self):
-        print("удаление экхемпляра" + str(self))
-
-pt = Point(1,3)
-
-print(pt.__dict__)
+    # def __del__(self): #Необязательный метод
+    #     print("удаление экхемпляра" + str(self))
